61a7a090527fc238ca1eff0a722c6c487899735e
Cnidarian <cmphillips23@gmail.com>
2021-01-26 21:28:25 -0600

    created variables to store price of flights, as well as arrival/departure times

--- flights.js
+++ flights.js
@@ -4,19 +4,19 @@ var returnDate = "2021-02-07";
 var originCode = "IAH";
 var destCode = "JFK";
 
-var getResults = function() {
+var getAuth = function() {
     var urlencoded = new URLSearchParams();
     urlencoded.append("client_id", config.flight_api_id);
     urlencoded.append("client_secret", config.flight_api_secret);
     urlencoded.append("grant_type", "client_credentials");
 
-    var requestOptions = {
+    var requestAuth = {
     method: 'POST',
     body: urlencoded,
     redirect: 'follow'
     };
-
-    fetch("https://test.api.amadeus.com/v1/security/oauth2/token", requestOptions)
+    
+    fetch("https://test.api.amadeus.com/v1/security/oauth2/token", requestAuth)
     .then(response => response.json())
     .then(function(response) {
         var bearerToken = response.access_token;
@@ -35,8 +35,31 @@ var flightResults = function(bearerToken) {
       
     fetch("https://test.api.amadeus.com/v2/shopping/flight-offers?originLocationCode=" + originCode + "&destinationLocationCode=" + destCode + "&departureDate=" + departDate + "&returnDate=" + returnDate + "&adults=1&max=5", requestOptions)
         .then(response => response.json())
-        .then(result => console.log(result))
+        .then(function(response) {
+            console.log(response);
+            var ticketPrices = [];
+            var departTimes = [];
+            var arriveTimes = [];
+
+            for (i = 0; i < response.data.length; i++) {
+                ticketPrices.push(response.data[i].price.total);
+            };
+            console.log(ticketPrices);
+
+            for (i = 0; i < response.data.length; i++) {
+                departTimes.push(response.data[i].itineraries[0].segments[0].departure.at);
+                departTimes[i] = departTimes[i].slice(11,16);
+            };
+            console.log(departTimes);
+            
+            for (i = 0; i < response.data.length; i++) {
+                arriveTimes.push(response.data[i].itineraries[0].segments[0].arrival.at);
+                arriveTimes[i] = arriveTimes[i].slice(11,16);
+            };
+            console.log(arriveTimes);
+        })
         .catch(error => console.log('error', error));
+
 };
 
-tokenBtn.addEventListener("click", getResults);
+tokenBtn.addEventListener("click", getAuth);

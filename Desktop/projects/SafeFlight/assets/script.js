bc6eb42bb148b807443bbb32d6847070ba396759
Nate Schroeder <n8dogg59@hotmail.com>
2021-02-04 21:30:42 -0600

    cleaned up charts

--- Desktop/projects/SafeFlight/assets/script.js
+++ Desktop/projects/SafeFlight/assets/script.js
@@ -191,44 +191,45 @@ var cityStateArr = [{"name": "Alabama", "abbreviation": "AL"},
                     return response.json().then(function (response) {
                         //console.log(response);
                         var dailyCases = response[1].positiveIncrease;
-                        var weekOneTotalCases = 0;
-                        var weekTwoTotalCases = 0;
-                        var weekThreeTotalCases = 0;
-                        var weekFourTotalCases = 0;
-                        var weekFiveTotalCases = 0;
-                        var weekOneDeathIncrease = 0;
-                        var weekTwoDeathIncrease = 0;
-                        var weekThreeDeathIncrease = 0;
-                        var weekFourDeathIncrease = 0;
-                        var weekFiveDeathIncrease = 0;
+                        var week1TotalCases = 0;
+                        var week2TotalCases = 0;
+                        var week3TotalCases = 0;
+                        var week4TotalCases = 0;
+                        var week5TotalCases = 0;
+                        var week1DeathIncrease = 0;
+                        var week2DeathIncrease = 0;
+                        var week3DeathIncrease = 0;
+                        var week4DeathIncrease = 0;
+                        var week5DeathIncrease = 0;
                         for (i = 0; i < 35; i++) {
                             if (i < 7) {
-                                var weekOneTotalCases = weekOneTotalCases + response[i].positiveIncrease;
-                                var weekOneDeathIncrease = weekOneDeathIncrease + response[i].deathIncrease;
+                                var week1TotalCases = week1TotalCases + response[i].positiveIncrease;
+                                var week1DeathIncrease = week1DeathIncrease + response[i].deathIncrease;
                             } else if (i > 6 && i < 14) {
-                                var weekTwoTotalCases = weekTwoTotalCases + response[i].positiveIncrease;
-                                var weekTwoDeathIncrease = weekTwoDeathIncrease + response[i].deathIncrease;
+                                var week2TotalCases = week2TotalCases + response[i].positiveIncrease;
+                                var week2DeathIncrease = week2DeathIncrease + response[i].deathIncrease;
                             } else if (i > 13 && i < 21) {
-                                var weekThreeTotalCases = weekThreeTotalCases + response[i].positiveIncrease;
-                                var weekThreeDeathIncrease = weekThreeDeathIncrease + response[i].deathIncrease;
+                                var week3TotalCases = week3TotalCases + response[i].positiveIncrease;
+                                var week3DeathIncrease = week3DeathIncrease + response[i].deathIncrease;
                             } else if (i > 20 && i < 28) {
-                                var weekFourTotalCases = weekFourTotalCases + response[i].positiveIncrease;
-                                var weekFourDeathIncrease = weekFourDeathIncrease + response[i].deathIncrease;
+                                var week4TotalCases = week4TotalCases + response[i].positiveIncrease;
+                                var week4DeathIncrease = week4DeathIncrease + response[i].deathIncrease;
                             } else if (i > 27 && i < 35) {
-                                var weekFiveTotalCases = weekFiveTotalCases + response[i].positiveIncrease;
-                                var weekFiveDeathIncrease = weekFiveDeathIncrease + response[i].deathIncrease;
+                                var week5TotalCases = week5TotalCases + response[i].positiveIncrease;
+                                var week5DeathIncrease = week5DeathIncrease + response[i].deathIncrease;
                             }
                         }
-                        document.getElementById("week1Cases").innerHTML = weekOneTotalCases;                        
-                        document.getElementById("week2Cases").innerHTML = weekTwoTotalCases;
-                        document.getElementById("week3Cases").innerHTML = weekThreeTotalCases;
-                        document.getElementById("week4Cases").innerHTML = weekFourTotalCases;
-                        document.getElementById("week5Cases").innerHTML = weekFiveTotalCases;
-                        document.getElementById("week1Deaths").innerHTML = weekOneDeathIncrease;                        
-                        document.getElementById("week2Deaths").innerHTML = weekTwoDeathIncrease;
-                        document.getElementById("week3Deaths").innerHTML = weekThreeDeathIncrease;
-                        document.getElementById("week4Deaths").innerHTML = weekFourDeathIncrease;
-                        document.getElementById("week5Deaths").innerHTML = weekFiveDeathIncrease;
+                                                
+                        document.getElementById("week1Cases").innerHTML = week1TotalCases;                        
+                        document.getElementById("week2Cases").innerHTML = week2TotalCases;
+                        document.getElementById("week3Cases").innerHTML = week3TotalCases;
+                        document.getElementById("week4Cases").innerHTML = week4TotalCases;
+                        document.getElementById("week5Cases").innerHTML = week5TotalCases;
+                        document.getElementById("week1Deaths").innerHTML = week1DeathIncrease;                        
+                        document.getElementById("week2Deaths").innerHTML = week2DeathIncrease;
+                        document.getElementById("week3Deaths").innerHTML = week3DeathIncrease;
+                        document.getElementById("week4Deaths").innerHTML = week4DeathIncrease;
+                        document.getElementById("week5Deaths").innerHTML = week5DeathIncrease;
 
                         var table = document.getElementById('dataTable');
                         var json = []; // First row needs to be headers 
@@ -279,9 +280,24 @@ var cityStateArr = [{"name": "Alabama", "abbreviation": "AL"},
                                 events:["click"],
                                 responsive: true, // Instruct chart js to respond nicely.
                                 maintainAspectRatio: false, // Add to prevent default behavior of full-width/height 
+                                scales: {
+                                    yAxes: [{
+                                        ticks: {
+                                            beginAtZero:false,
+                                            callback: function(value, index, values) {
+                                                if(parseInt(value) >= 1000){
+                                                   return value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+                                                } else {
+                                                   return value;
+                                                }
+                                           }                            
+                                        }
+                                    }]
+                                }
                                 }
                             });
                             console.log(myChart);
+                            console.log(values);
                             return myChart;
                             }
 
@@ -317,6 +333,26 @@ var cityStateArr = [{"name": "Alabama", "abbreviation": "AL"},
                                 events:["click"],
                                 responsive: true, // Instruct chart js to respond nicely.
                                 maintainAspectRatio: false, // Add to prevent default behavior of full-width/height 
+                                scales: {
+                                    yAxes: [{
+                                        ticks: {
+                                            beginAtZero:false,
+                                            callback: function(value, index, values) {
+                                                if(parseInt(value) >= 1000){
+                                                   return value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+                                                } else {
+                                                   return value;
+                                                }
+                                           }                            
+                                        }
+                                    }],
+                                    xAxes: [{
+                                        offset: true
+                                    }]
+                                }
+                                
+                                    
+                                
                                 }
                             });
                             console.log(myChart);
